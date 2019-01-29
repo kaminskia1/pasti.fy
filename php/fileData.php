a68b53b8d0fca02f9d57891dbc236b40eccc792d
kaminskia1 <8168918+kaminskia1@users.noreply.github.com>
2019-01-28 22:52:45 -0500

    Update fileData.php

--- php/fileData.php
+++ php/fileData.php
@@ -1,5 +1,5 @@
 <?php
 function fileToVar($file) {
-	return file_get_contents("/var/www/vhosts/testing.cheapboost.me/httpdocs/AppSpot/app/pastify/data/" . $file, FILE_USE_INCLUDE_PATH);
+	return file_get_contents("[data location]" . $file, FILE_USE_INCLUDE_PATH);
 }
 ?>
